928fb39b0872b98959388c602e0c321be13a7fb4
MuriloPereiraDEV <102379940+MuriloPereiraDEV@users.noreply.github.com>
2022-10-21 23:19:50 -0300

    Change structure

--- bancoDeDados.py
+++ bancoDeDados.py
@@ -1,22 +1,15 @@
 import pyodbc
 import pandas as pd
-
-date = '2022-10-01'
+from credenciais import SERVER, DATABASE, USERNAME, PASSWORD, DATE
 
 def bancoSql():
-    #Dados de Conexão
-    server = '18.223.91.130'
-    #database = 'SBO_Softex'
-    database = 'fusionProd'
-    username = 'sa'
-    password = 'SOF@2018'
     
     #Conexão ao Banco
-    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+SERVER+';DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD)
     print("Conexão realizada")
     cursor = cnxn.cursor()
-    #
-    query = "SELECT [datadopagamento],[codigo],[fornecedorrelatorio],[CPFCNPJ],[itemDeDespesa],[projetorelatorio], [valordasolicitacao]  FROM dbo.D_PagamentoPrincipal WHERE  retornoSAP = 1 AND datadopagamento > '"+ date +"';"
+    
+    query = "SELECT [datadopagamento],[codigo],[fornecedorrelatorio],[CPFCNPJ],[itemDeDespesa],[projetorelatorio], [valordasolicitacao]  FROM dbo.D_PagamentoPrincipal WHERE  retornoSAP = 1 AND datadopagamento > '"+ DATE +"';"
     df = pd.read_sql(query, cnxn)
     dados = pd.DataFrame(df)
 
